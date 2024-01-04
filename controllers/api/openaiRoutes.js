64e5a06d1f6a651f5ceea02b753992830b677a56
unknown <elsie.dh@gmail.com>
2024-01-04 00:36:33 -0800

    image generating from input prompt

--- controllers/api/openaiRoutes.js
+++ controllers/api/openaiRoutes.js
@@ -1,8 +1,38 @@
 const express = require('express');
-const { generateImage } = require('../openaiController');
+const { OpenAI } = require("openai");
+//const cors = require("cors");
+//const { generateImage } = require('../openaiController');
 const router = express.Router();
+require("dotenv").config();
+//const { Configuration, OpenAIApi } = require("openai");
+
+const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY,
+  }); 
+//router.post('/generateimage', generateImage,) 
 
 
-router.post('/generateimage', generateImage,) 
+const generateImage = async (prompt) => {
+    const response = await openai.images.generate ({
+        prompt: prompt,
+        n: 1,
+        size: "256x256",
+        
+    });
+    console.log(response)
+    /* const image = response.data;
+    return image; */
+    
+    
+}
+
+
+
+router.post('/generateImage', async (req, res) => {
+    const image = await generateImage(req.body.prompt);
+    res.send({image});
+});
+
+
 
 module.exports = router;
